0117d31b6c80ef5e4c0d38bdc0e7c49ad59d5b44
naren4488 <narendrakajla77@gmail.com>
2024-03-21 18:03:34 +0530

    Implemented Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,17 @@
+import logoIcon from "@/assets/food-logo-colored.png";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className=" bg-black px-6  py-4 text-white sm:px-20">
+      <div className="flex  items-center gap-2">
+        <img src={logoIcon} alt="Logo" className="size-10 " />
+        <h2 className="text-2xl font-bold">FoodMenuApp</h2>
+      </div>
+      <p className=" mt-2 text-sm text-slate-200">
+        @2024 all rights are reserved for any copyrights
+      </p>
+    </div>
+  );
 };
 
 export default Footer;
--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -4,9 +4,9 @@ const Logo = ({ imgURL }: { imgURL: string }) => {
   return (
     <Link to={"#"} className="flex cursor-pointer items-center gap-4">
       <img src={imgURL} alt="Logo" className="size-10 " />
-      <h1 className="text-2xl font-bold text-orange-500 max-sm:hidden">
+      <h2 className="text-2xl font-bold text-orange-500 max-sm:hidden">
         FoodMenuApp
-      </h1>
+      </h2>
     </Link>
   );
 };
